9c77d2fa231dc223f0b93d692424a213e2876367
meblo <chmiel-97@o2.pl>
2023-02-23 18:07:03 -1200

    first commit

---
+++ src/main/java/DbUtil.java
@@ -0,0 +1,25 @@
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DbUtil {
+    private static DataSource dataSource;
+    public static Connection getConnection() throws SQLException {
+        return getIstance().getConnection();
+    }
+    private static DataSource getIstance() {
+        if (dataSource == null) {
+            try {
+                Context initContext = new InitialContext();
+                Context envContext = (Context) initContext.lookup("java:/comp?env");
+                dataSource = (DataSource)envContext.lookup("jdbc/users");
+            }catch (NamingException e ) {
+                e.printStackTrace();
+            }
+        }
+        return dataSource;
+    }
+}
